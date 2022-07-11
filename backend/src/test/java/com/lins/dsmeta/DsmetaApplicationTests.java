253aa35e3f35d0d39bedb66456ed7318f38a88dc
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-07-11 13:40:28 -0300

    Project created

---
+++ backend/src/test/java/com/lins/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lins.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
