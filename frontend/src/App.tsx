2b8320f1d1b667a54b6b2f86fef1c0c04f245ab1
Marcelo Lins <marcelolinsdemedeiros@gmail.com>
2022-07-11 21:15:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
+    <>
     <h1>Olá Mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
